cc9d64ce2982fc3051d4d6bb94d613a1918097bb
mausam-mantracare <mausamgirimatracare@gmail.com>
2025-02-04 17:12:19 +0530

    fix

--- print-queue/puzzle1.js
+++ print-queue/puzzle1.js
@@ -4,10 +4,9 @@ const data = readFileSync(process.argv[2], "utf-8");
 const [rules, queue] = data.split("\n\n")
 
 const ruleMap = {}
-const target = rules.split("\n")
-target.forEach((t) => {
+rules.split("\n").forEach((t) => {
     const [x, y] = t.split("|").map(Number)
-    if(ruleMap[x] == undefined) ruleMap[x] = new Set();
+    if (ruleMap[x] == undefined) ruleMap[x] = new Set();
     ruleMap[x].add(y)
     // if (!prev) ruleMap.set(y, [x])
     // else ruleMap.set(y, [...prev, x])
@@ -17,23 +16,23 @@ target.forEach((t) => {
 const queue_list = queue.split("\n").map(n => n.split(",").map(Number));
 // console.log(queue_list);
 
-function checkQueueRule(queue, rule){
-    for(let i = 0; i < queue.length; i++){
-        for(let j = i + 1; j < queue.length; j++){
+function checkQueueRule(queue, rule) {
+    for (let i = 0; i < queue.length; i++) {
+        for (let j = i + 1; j < queue.length; j++) {
             const left = queue[i]
             const right = queue[j]
 
             // console.log(rule[left], rule[right])
-            if(!rule[left]?.has(right)) return false
+            if (!rule[left]?.has(right)) return false
         }
     }
     return true
 }
 
-const validQueue = queue_list.filter(queue => 
-   checkQueueRule(queue, ruleMap))
+const validQueue = queue_list.filter(queue =>
+    checkQueueRule(queue, ruleMap))
 
-function findMiddle(arr){
+function findMiddle(arr) {
     return arr[Math.floor(arr.length / 2)]
 }
 // console.log(validQueue)
